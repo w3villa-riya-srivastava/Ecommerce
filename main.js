bd7066f90ed2ff887050a63460339a84c98b14eb
Riya Srivastava <riya.srivastava@w3villa.com>
2024-06-10 16:51:32 +0530

    Carousels Added

--- main.js
+++ main.js
@@ -35,3 +35,43 @@ $('.slider').slick({
       // instead of a settings object
     ]
   });
+
+// BRAND CAROUSEL
+
+$('.brand-slide').slick({
+    dots: true,
+    infinite: true, //if TRUE slides will keep moving and will return to first slide otheriwse it will not move after showing all slides
+    speed: 300,
+    slidesToShow: 9, 
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
