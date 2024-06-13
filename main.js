09dd4a1e3add530e1e93ed666f4a67412c1a78e9
Riya Srivastava <riya.srivastava@w3villa.com>
2024-06-13 18:31:28 +0530

    updated

--- main.js
+++ main.js
@@ -46,10 +46,19 @@ $('.brand-slide').slick({
     slidesToScroll: 3,
     responsive: [
       {
+        breakpoint: 1530,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
         breakpoint: 1199,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 4,
+          slidesToScroll: 4,
           infinite: true,
           dots: true
         }
@@ -70,8 +79,43 @@ $('.brand-slide').slick({
           arrows: false
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+    ]
+  });
+
+
+  // LEFT-CAROUSEL SLIDE 
+
+  $('.one-slide').slick({
+    dots: true,
+    infinite: true, //if TRUE slides will keep moving and will return to first slide otheriwse it will not move after showing all slides
+    speed: 300,
+    slidesToShow: 1, 
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 928,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      }
     ]
   });
